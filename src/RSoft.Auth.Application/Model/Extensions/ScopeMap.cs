709f0da28de928789450acb33ba63ffa8f6cced3
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2020-09-10 23:15:39 -0300

    Added scope application service and map

---
+++ src/RSoft.Auth.Application/Model/Extensions/ScopeMap.cs
@@ -0,0 +1,79 @@
+﻿using RSoft.Auth.Domain.Entities;
+
+namespace RSoft.Auth.Application.Model.Extensions
+{
+
+    /// <summary>
+    /// Contains methods for Scope entity-dto mapping
+    /// </summary>
+    public static class ScopeMap
+    {
+
+        /// <summary>
+        /// Map dto to entity
+        /// </summary>
+        /// <param name="dto">Object to extension</param>
+        public static Scope Map(this ScopeDto dto)
+        {
+            return new Scope()
+            {
+                Name = dto.Name,
+                IsActive = dto.IsActive
+            };
+        }
+
+        /// <summary>
+        /// Map entity to dto
+        /// </summary>
+        /// <param name="entity">Object to extension</param>
+        /// <param name="dto">Scope Dto object</param>
+        public static Scope Map(this Scope entity, ScopeDto dto)
+        {
+            if (dto != null)
+            {
+                entity.Name = dto.Name;
+                entity.IsActive = dto.IsActive;
+            }
+            return entity;
+        }
+
+        /// <summary>
+        /// Map entity to dto
+        /// </summary>
+        /// <param name="Scope">Object to extension</param>
+        public static ScopeDto Map(this Scope Scope)
+            => Map(Scope, true);
+
+        /// <summary>
+        /// Map entity to dto
+        /// </summary>
+        /// <param name="entity">Object to extension</param>
+        /// <param name="addAuthors">Indicate if add authors data in dto</param>
+        public static ScopeDto Map(this Scope entity, bool addAuthors)
+        {
+            
+            ScopeDto dto = new ScopeDto();
+
+            if (entity.Valid)
+            {
+
+                dto.Id = entity.Id;
+                if (addAuthors)
+                    Author.Map(dto, entity);
+
+            }
+            else
+            {
+                dto.AddNotifications(entity.Notifications);
+            }
+
+            dto.Name = entity.Name;
+            dto.IsActive = entity.IsActive;
+
+            return dto;
+
+        }
+
+    }
+
+}
---
+++ src/RSoft.Auth.Application/Model/ScopeDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using RSoft.Framework.Application.Model;
+
+namespace RSoft.Auth.Application.Model
+{
+
+    /// <summary>
+    /// Scope data transport object
+    /// </summary>
+    public class ScopeDto : AppDtoIdAuditBase<Guid>, IAuditDto<Guid>
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// Indicate if entity is active
+        /// </summary>
+        public bool IsActive { get; set; }
+
+        /// <summary>
+        /// Entity name value
+        /// </summary>
+        public string Name { get; set; }
+
+        #endregion
+
+    }
+
+}
