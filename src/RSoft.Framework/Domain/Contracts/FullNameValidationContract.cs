479a53f49b3e0dcab5b9a668bb7180864bf47711
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2020-09-14 22:24:33 -0300

    Added parameters to FullNameValidationContract

--- src/RSoft.Framework/Domain/Contracts/FullNameValidationContract.cs
+++ src/RSoft.Framework/Domain/Contracts/FullNameValidationContract.cs
@@ -5,9 +5,6 @@ namespace RSoft.Framework.Domain.Contracts
 
     /// <summary>
     /// Full name validation contract.
-    /// First name and last name are required.
-    /// First name must between 2 and 50 characters.
-    /// Last name must between 2 and 100 characters
     /// </summary>
     public class FullNameValidationContract : BaseValidationContract
     {
@@ -18,33 +15,39 @@ namespace RSoft.Framework.Domain.Contracts
         /// Create a new instance of object
         /// </summary>
         /// <param name="name">Name object instance</param>
-        public FullNameValidationContract(IFullName name) : this(name, string.Empty) { }
+        public FullNameValidationContract(IFullName name) : this(name, new FullNameValidationArgument()
+        {
+            CharListAllowed = string.Empty,
+            FirstNameMinimumLength = 2,
+            FirstNameMaximumLength = 50,
+            LastNameMinimumLength = 2,
+            LastNameMaximumLength = 100
+        }) { }
 
         /// <summary>
         /// Create a new instance of object
         /// </summary>
         /// <param name="name">Name object instance</param>
-        /// <param name="charListAllowed">Char list of allowed characters</param>
-        public FullNameValidationContract(IFullName name, string charListAllowed)
+        /// <param name="args">Full name validation arguments</param>
+        public FullNameValidationContract(IFullName name, FullNameValidationArgument args)
         {
 
             // Regular expression for all characteres name (global)
             // ^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$
 
             //TODO: Globalization
-            //TODO: Add characters len parameter
             Contract
                 .Requires()
                 
                 .IsNotNullOrEmpty(name.FirstName, "First name", "First name is required")
-                .HasMinLen(name.FirstName ?? string.Empty, 2, "First name", "First name must contain at least 2 characters")
-                .HasMaxLen(name.FirstName ?? string.Empty, 50, "Last name", "First name must contain a maximum of 50 characters")
-                .Matchs(name.FirstName, $"^[a-zA-Z{charListAllowed} ,.'-]+$", "First name", "First name contains invalid characters")
+                .HasMinLen(name.FirstName ?? string.Empty, args.FirstNameMinimumLength, "First name", "First name must contain at least 2 characters")
+                .HasMaxLen(name.FirstName ?? string.Empty, args.FirstNameMaximumLength, "Last name", "First name must contain a maximum of 50 characters")
+                .Matchs(name.FirstName, $"^[a-zA-Z{args.CharListAllowed} ,.'-]+$", "First name", "First name contains invalid characters")
                 
                 .IsNotNullOrEmpty(name.LastName, "Last name", "Last name is required")
-                .HasMinLen(name.LastName ?? string.Empty, 2, "Last name", "Last name must contain at least 2 characters")
-                .HasMaxLen(name.LastName ?? string.Empty, 100, "Last name", "Last name must contain a maximum of 50 characters")
-                .Matchs(name.FirstName, $"^[a-zA-Z{charListAllowed} ,.'-]+$", "Last name", "Last name contains invalid characters")
+                .HasMinLen(name.LastName ?? string.Empty, args.LastNameMinimumLength, "Last name", "Last name must contain at least 2 characters")
+                .HasMaxLen(name.LastName ?? string.Empty, args.LastNameMaximumLength, "Last name", "Last name must contain a maximum of 50 characters")
+                .Matchs(name.FirstName, $"^[a-zA-Z{args.CharListAllowed} ,.'-]+$", "Last name", "Last name contains invalid characters")
             ;
 
         }
