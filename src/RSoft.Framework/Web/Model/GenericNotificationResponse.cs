72f081b89698a78c36d1b9e7b4dbbad0d02f2ab4
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2020-09-19 19:33:18 -0300

    Added Web.Api layer abstracts object in framework

---
+++ src/RSoft.Framework/Web/Model/GenericNotificationResponse.cs
@@ -0,0 +1,41 @@
+﻿namespace RSoft.Framework.Web.Model
+{
+
+    /// <summary>
+    /// Generic notification response model
+    /// </summary>
+    public class GenericNotificationResponse
+    {
+
+        #region Constructors
+
+        /// <summary>
+        /// Create a new GenericNotificationResponse instance
+        /// </summary>
+        /// <param name="property">Property/Field name</param>
+        /// <param name="message">Message detail</param>
+        public GenericNotificationResponse(string property, string message)
+        {
+            Property = property;
+            Message = message;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Property/FIeld name
+        /// </summary>
+        public string Property { get; set; }
+
+        /// <summary>
+        /// Message detail
+        /// </summary>
+        public string Message { get; set; }
+
+        #endregion
+
+    }
+
+}
