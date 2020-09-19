f5cb79f53d30e759149bdea1c4a0b035d706fe9c
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2020-09-19 20:02:08 -0300

    Start dev Auth API

---
+++ src/RSoft.Auth.Web.Api/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RSoft.Framework.Web.Api;
+
+namespace RSoft.Auth.Web.Api.Controllers
+{
+
+    /// <summary>
+    /// API Authetication
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ApiBaseController
+    {
+    }
+}
