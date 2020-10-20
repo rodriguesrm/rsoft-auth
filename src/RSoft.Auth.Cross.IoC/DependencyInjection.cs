5003c16d4d88ba07ff89cf23ed209a82e474c548
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2020-10-20 07:16:30 -0300

    Start dev globalization in web.api

--- src/RSoft.Auth.Cross.IoC/DependencyInjection.cs
+++ src/RSoft.Auth.Cross.IoC/DependencyInjection.cs
@@ -36,6 +36,7 @@ namespace RSoft.Auth.Cross.IoC
             services.Configure<SecurityOptions>(options => configuration.GetSection("Application:Security").Bind(options));
             services.Configure<RSApiOptions>(options => configuration.GetSection("Application:RSoftApis").Bind(options));
             services.Configure<CredentialOptions>(options => configuration.GetSection("Application:Credential").Bind(options));
+            services.Configure<CultureOptions>(options => configuration.GetSection("Application:Culture").Bind(options));
 
             #endregion
 
--- src/RSoft.Auth.Web.Api/Startup.cs
+++ src/RSoft.Auth.Web.Api/Startup.cs
@@ -1,20 +1,29 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using RSoft.Auth.Cross.Common.Options;
 using RSoft.Auth.Cross.IoC;
 using RSoft.Auth.Infra.Data.Extensions;
 using RSoft.Auth.Web.Api.Extensions;
 using RSoft.Auth.Web.Api.Helpers;
+using RSoft.Auth.Web.Api.Languages;
 using RSoft.Auth.Web.Api.Policies;
 using RSoft.Framework.Web.Extensions;
 using RSoft.Framework.Web.Filters;
 using RSoft.Logs.Extensions;
 using RSoft.Logs.Middleware;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Reflection;
 
 namespace RSoft.Auth.Web.Api
@@ -69,6 +78,31 @@ namespace RSoft.Auth.Web.Api
             services.AddApplicationHealthChecks(Configuration);
             services.AddScoped<ITokenHelper, TokenHelper>();
 
+            #region Language/Culture
+
+            services.AddLocalization();
+
+            services.Configure<RequestLocalizationOptions>(
+                options =>
+                {
+                    CultureOptions cultureOptions = new CultureOptions();
+                    Configuration.GetSection("Application:Culture").Bind(cultureOptions);
+
+                    IList<CultureInfo> supportedCultures = cultureOptions.SupportedLanguage.Select(c => new CultureInfo(c)).ToList();
+
+                    options.DefaultRequestCulture = new RequestCulture(culture: cultureOptions.DefaultLanguage, uiCulture: cultureOptions.DefaultLanguage);
+                    options.SupportedCultures = supportedCultures;
+                    options.SupportedUICultures = supportedCultures;
+                    options.RequestCultureProviders = new[] { new RouteDataRequestCultureProvider { IndexOfCulture = 3, IndexofUICulture = 3 } };
+                });
+
+            services.Configure<RouteOptions>(options =>
+            {
+                options.ConstraintMap.Add("culture", typeof(LanguageRouteConstraint));
+            });
+
+            #endregion
+
         }
 
         /// <summary>
@@ -99,6 +133,13 @@ namespace RSoft.Auth.Web.Api
             app.UseSwaggerDocUI(provider);
             app.UseApplicationHealthChecks();
 
+            #region Language/Culture
+
+            IOptions<RequestLocalizationOptions> localizeOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(localizeOptions.Value);
+
+            #endregion
+
             app.UseRouting();
 
             app.UseAuthentication();
@@ -107,6 +148,7 @@ namespace RSoft.Auth.Web.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapControllerRoute("default", "{culture:culture}/{controller=Home}/{action=Index}/{id?}"); //Language/Culture
             });
 
             ILogger logger = factory.CreateLogger("Microsoft.Hosting.Lifetime");
